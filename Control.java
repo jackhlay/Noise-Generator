acb830f3aa1cf32910c88265ed0c30ea3889c541
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2022-05-02 20:53:37 -0600

    Cleanup
    removed unnecessary instantiations

--- Control.java
+++ Control.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 
 public class Control {
-    Main m = new Main();
-    Sort_Quantify s = new Sort_Quantify();
-
+    
     public static void main(String[] args) throws IOException {
         new Main().go();
     }
