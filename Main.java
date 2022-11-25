4aea3552967e4f5cfb95e239da32b1f7f219ed7d
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2022-11-24 20:21:32 -0700

    GUI
    made a rough GUI with fields allowing the user to customize the image to their height, width and # of points. Cleaned up the files as well.

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,7 +26,7 @@ public class Main extends JPanel{
         }
         ArrayList<coordinate> clst = new ArrayList<coordinate>(q);
 
-        BufferedImage canvas = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage canvas = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 
         JFrame frame = new JFrame() {
             {
@@ -55,6 +58,51 @@ public class Main extends JPanel{
     }
     public void start(){
         JFrame start = new JFrame("Settings Configurator");
-        start.add()
+        JPanel container = new JPanel();
+        container.setVisible(true);
+        start.setSize(600,600);
+        JLabel l1 = new JLabel("# of attempts");
+        JLabel l2 = new JLabel("Image Height");
+        JLabel l3 = new JLabel("Image Width");
+
+        JTextField T1 = new JTextField();
+        JTextField T2 = new JTextField();
+        JTextField T3 = new JTextField();
+
+        JButton send = new JButton("Generate");
+
+        start.setLayout(new BorderLayout());
+        start.add(send, BorderLayout.SOUTH);
+        start.add(container, BorderLayout.CENTER);
+        container.setLayout(new GridLayout(3,2));
+        container.add(l1);
+        container.add(T1);
+        container.add(l2);
+        container.add(T2);
+        container.add(l3);
+        container.add(T3);
+
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num = Integer.parseInt(T1.getText());
+                int h = Integer.parseInt(T2.getText());
+                int w = Integer.parseInt(T3.getText());
+                try {
+                    go(num, h, w);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        start.setVisible(true);
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().start();
+
     }
 }
